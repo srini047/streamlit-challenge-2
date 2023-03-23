68bd4118a252f155db28dc0949bd0725a8ef63e5
srini047 <srinikethcr7@gmail.com>
2023-03-24 00:29:56 +0530

    update: save changes

--- answer.py
+++ answer.py
@@ -2,10 +2,10 @@ import os
 import openai
 
 def generate_answer(question):
-    openai.api_key =  os.getenv("OPENAI_API_KEY")
+    openai.api_key =  st.secrets["OPENAI_KEY"]
     response = openai.Completion.create(
     model="text-davinci-003",
-    prompt=question,
+    prompt='Please generate answer for the question: ' + question,
     temperature=0.7,
     max_tokens=256,
     top_p=1.0,
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 data = pd.read_csv('./jina-faq.csv')
 
-st.title('Dashboard')
+st.title('FAQ answer generator')
 edited_df = st.experimental_data_editor(
     data,
     num_rows="dynamic",
