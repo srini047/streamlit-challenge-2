0670b34fb02ba2ce4c998099027b258bc6dab645
srini047 <srinikethcr7@gmail.com>
2023-03-24 23:07:28 +0530

    add: file uploader

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 data = pd.read_csv('./jina-faq.csv')
 
+# Uplaod file
+uploaded_file = st.file_uploader('Please upload your FAQ\'s file with only one column of FAQs', type=['.csv'], accept_multiple_files=False, label_visibility="visible")
+
+# Read data
+data = pd.read_csv(uploaded_file)
+
+# Show the editor
 st.title('FAQ answer generator')
 edited_df = st.experimental_data_editor(
     data,
